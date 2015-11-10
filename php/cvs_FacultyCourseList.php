d426af769ca3f7585cdaa6c7d87604be5b76e182
rich <rich@10.161.54.130>
2015-11-09 16:40:56 -0800

    update AD login

--- php/cvs_FacultyCourseList.php
+++ php/cvs_FacultyCourseList.php
@@ -2,11 +2,6 @@
     require("config.php");
 
     $TermCode = filter_input(INPUT_GET, 'TermCode');
-    
-//    $query = "SELECT 'Instructor' AS UserType, empl.Title, empl.FirstName, empl.LastName, empl.Email, svcr.CourseTitle, svcr.SectionNum, svcr.CourseID, '1' AS CourseType, "
-//            . "(SELECT COUNT(StudentID) FROM [TOPSPIN.SOCCCD.EDU\TOPSPIN].[Tardis].[dbo].[StudentCourses] WHERE TermCode = '".$TermCode."' AND SectionNum = svcr.SectionNum) AS Participants "
-//            . "FROM [IVCFASV].[dbo].[SurveyCourse] AS svcr LEFT JOIN [TOPSPIN.SOCCCD.EDU\TOPSPIN].[Tardis].[dbo].[EmployeeInfo] AS empl ON svcr.InstructorUID = empl.UserID "
-//            . "WHERE svcr.TermCode = '".$TermCode."' AND svcr.OptOut = 0";
 
     $dbConn->setAttribute(constant('PDO::SQLSRV_ATTR_DIRECT_QUERY'), true);
     
@@ -53,7 +48,7 @@
     $out = fopen("php://output", 'w+');
 
     // Write the spreadsheet column titles / labels
-    fputcsv($out, array('usertype','title','firstname', 'surename', 'email', 'course_name', 'course_code', 'program_of_studies', 'course_type', 'course_participants'));
+    fputcsv($out, array('usertype','title','firstname', 'surname', 'email', 'course_name', 'course_code', 'program_of_studies', 'course_type', 'course_participants'));
     // Write all the records to the spreadsheet
     foreach($data as $row) {
         fputcsv($out, array($row['UserType'], $row['Title'], $row['FirstName'], $row['LastName'], $row['Email'], $row['CourseTitle'], $row['SectionNum'], $row['CourseID'], $row['CourseType'], $row['Participants']));
--- php/db_insertSurveyCourseFromTardis.php
+++ php/db_insertSurveyCourseFromTardis.php
@@ -2,14 +2,36 @@
     require("config.php");
     
     $TermCode = filter_input(INPUT_POST, 'TermCode');
-
-    $query = "INSERT INTO [IVCFASV].[dbo].[SurveyCourse] (TermCode, InstructorUID, SectionNum, CourseID, CourseTitle, CourseDescription) "
-            . "SELECT	TermCode, InstructorUID, SectionNum, CourseID, CourseTitle, CourseDescription "
+    
+    $dbConn->setAttribute(constant('PDO::SQLSRV_ATTR_DIRECT_QUERY'), true);
+    
+    $query1 = "CREATE TABLE #RESULT1 (TermCode nvarchar(255), InstructorUID nvarchar(255), SectionNum nvarchar(255), CourseID nvarchar(255), CourseTitle nvarchar(255), CourseDescription nvarchar(255))";
+    $query2 = "INSERT INTO #RESULT1 SELECT TermCode, InstructorUID, SectionNum, CourseID, CourseTitle, CourseDescription "
             . "FROM [TOPSPIN.SOCCCD.EDU\TOPSPIN].[Tardis].[dbo].[CourseInfo] "
             . "WHERE  CollegeCode = 'I' AND TermCode = '".$TermCode."'";
-
-    $cmd = $dbConn->prepare($query);
-    $cmd->execute(); 
+    
+    $query3 = "CREATE TABLE #RESULT2 (SectionNum nvarchar(255), Participants int)";
+    $query4 = "INSERT INTO #RESULT2 SELECT SectionNum, COUNT(StudentID) "
+            . "FROM [TOPSPIN.SOCCCD.EDU\TOPSPIN].[Tardis].[dbo].[StudentCourses] "
+            . "WHERE TermCode = '".$TermCode."' GROUP BY SectionNum";
+    
+    $query5 = "INSERT INTO [IVCFASV].[dbo].[SurveyCourse] (TermCode, InstructorUID, SectionNum, CourseID, CourseTitle, CourseDescription) "
+            . "SELECT rst1.TermCode, rst1.InstructorUID, rst1.SectionNum, rst1.CourseID, rst1.CourseTitle, rst1.CourseDescription "
+            . "FROM #RESULT1 AS rst1 INNER JOIN #RESULT2 AS rst2 ON rst1.SectionNum = rst2.SectionNum";
+    
+    $query6 = "DROP TABLE #RESULT1";
+    $query7 = "DROP TABLE #RESULT2";
+    
+    $dbConn->query($query1);
+    $dbConn->query($query2);
+    $dbConn->query($query3);
+    $dbConn->query($query4);
+    
+    $cmd = $dbConn->prepare($query5);
+    $cmd->execute();
     $data = $cmd->fetchAll();
+    
+    $dbConn->query($query6);
+    $dbConn->query($query7);
 
     echo json_encode($data);
